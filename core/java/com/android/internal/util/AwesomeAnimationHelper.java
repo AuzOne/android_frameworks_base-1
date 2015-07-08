eec523ddea0d89d1bb7a681a17407bbaf3ca82eb
Readone <android-armv7a-belalang-tempur@users.noreply.github.com>
2015-07-08 07:05:47 +0700

    fix

--- core/java/com/android/internal/util/AwesomeAnimationHelper.java
+++ core/java/com/android/internal/util/AwesomeAnimationHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.eos;
+package com.android.internal.util;
 
 import android.content.Context;
 import android.content.res.Resources;
